663a272d0016d63e6ad0fc7628c3b651c7ca0af1
miguelsarti <158165620+miguelsarti@users.noreply.github.com>
2024-10-31 10:57:10 -0300

    feat: create getById

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -22,6 +22,30 @@ usuariosRoutes.post("/", (req, res) => {
   });
 });
 
+usuariosRoutes.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const user = usersRepository.getUserById(id);
+
+  if (!user) {
+    return res.status(404).json({
+      message: `Usuário com o id ${id} não encontrado`,
+    });
+  }
+
+  return res.status(200).json({
+    message: `Usuário com o id ${id} encontrado`,
+    user,
+  });
+});
+
+
+usuariosRoutes.put("/:id", (req, res) => {
+
+})
+
+usuariosRoutes.delete("/:id", (req, res) => {
+
+})
 
 
 export default usuariosRoutes;
