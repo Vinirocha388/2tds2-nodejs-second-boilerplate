c69ef13563c488fd1b2d02ad42a86c05a595d53e
miguelsarti <158165620+miguelsarti@users.noreply.github.com>
2024-10-31 09:16:32 -0300

    feat: create UsersRepository

--- src/models/users/User.js
+++ src/models/users/User.js
@@ -11,3 +11,4 @@ class User {
     }
 }
 
+export default User;
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
+import UsersRepository from "../models/users/UsersRepository.js";
 
 const usuariosRoutes = Router();
+const usersRepository = new UsersRepository();
 
-let usuarios = [];
 
 usuariosRoutes.get("/", (req, res) => {
+  const usuarios = usersRepository.getAllUsers();
   return res.status(200).json({
     message: usuarios.length == 0 ? "Não há usuários cadastrados" : `Total de usuários cadastrados: ${usuarios.length}`,usuarios,
   });
